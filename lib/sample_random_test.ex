2825a107a97e15abfaec0108d9ce6c4e4612e69f
Susumu Yamazaki <zacky1972@gmail.com>
2023-11-10 19:24:05 +0900

    feat: implement function

--- lib/sample_random_test.ex
+++ lib/sample_random_test.ex
@@ -2,4 +2,23 @@ defmodule SampleRandomTest do
   @moduledoc """
   Documentation for `SampleRandomTest`.
   """
+
+  def function(x) do
+    {v, w} = singular_value()
+
+    unless x == v do
+      x
+    else
+      w
+    end
+  end
+
+  defp rand64() do
+    :rand.uniform(Bitwise.bsl(1, 64))
+  end
+
+  defp singular_value() do
+    :rand.seed(:exs1024s, {52, 259, 32})
+    {rand64(), rand64()}
+  end
 end
