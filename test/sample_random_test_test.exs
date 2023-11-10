e5434abac92e1086781db095b15e39fe7f14403f
Susumu Yamazaki <zacky1972@gmail.com>
2023-11-10 19:41:46 +0900

    test: implement random test

--- test/sample_random_test_test.exs
+++ test/sample_random_test_test.exs
@@ -1,4 +1,20 @@
 defmodule SampleRandomTestTest do
   use ExUnit.Case
   doctest SampleRandomTest
+
+  describe "function" do
+    test "simple test" do
+      assert SampleRandomTest.function(1) == 1
+    end
+
+    @tag random: true
+    test "random test" do
+      :rand.seed(:exs1024s)
+
+      1..10_000
+      |> Enum.map(fn _ -> :rand.uniform(Bitwise.bsl(1, 16)) end)
+      |> Enum.map(& {&1, SampleRandomTest.function(&1)})
+      |> Enum.map(fn {x, y} -> assert x == y end)
+    end
+  end
 end
