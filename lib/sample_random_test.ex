8d7aed013b19c26427c9eb174813dcf024e0f3f5
Susumu Yamazaki <zacky1972@gmail.com>
2023-11-10 19:14:21 +0900

    remove hello

--- lib/sample_random_test.ex
+++ lib/sample_random_test.ex
@@ -2,17 +2,4 @@ defmodule SampleRandomTest do
   @moduledoc """
   Documentation for `SampleRandomTest`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SampleRandomTest.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/sample_random_test_test.exs
+++ test/sample_random_test_test.exs
@@ -1,8 +1,4 @@
 defmodule SampleRandomTestTest do
   use ExUnit.Case
   doctest SampleRandomTest
-
-  test "greets the world" do
-    assert SampleRandomTest.hello() == :world
-  end
 end
