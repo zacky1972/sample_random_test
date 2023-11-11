d2bb1420b80a4b46bf9bb73c236fcca9197fa0a7
Susumu Yamazaki <zacky1972@gmail.com>
2023-11-12 06:19:28 +0900

    build: downgrade Elixir version of mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SampleRandomTest.MixProject do
     [
       app: :sample_random_test,
       version: "0.1.0",
-      elixir: "~> 1.16-rc",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
